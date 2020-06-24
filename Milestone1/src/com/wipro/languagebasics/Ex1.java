da2f63c6dd81be2bfceb5bd3a5c44f8f9bd512a1
Susrija <Susrija@DESKTOP-ILDABNP>
2020-06-24 12:24:24 +0530

    Java Programs on Language basics

--- Milestone1/src/com/wipro/languagebasics/Ex1.java
+++ Milestone1/src/com/wipro/languagebasics/Ex1.java
@@ -4,7 +4,6 @@ public class Ex1
 {
 	public static void main(String args[])
 	{
-		int a=Integer.parseInt(args[0]);
-		System.out.println(a);
+		 System.out.println(args[0] + " Technologies " + args[1]);
 	}
 }
