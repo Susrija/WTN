5c4e44222cb5e973ac395ea82342d5ae792539cb
Susrija <48689777+Susrija@users.noreply.github.com>
2020-06-24 12:45:25 +0530

    Create Ex3.java

---
+++ Milestone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+	public static void main(String args[])
+	{
+		int a,b,sum;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		sum = a+b;
+		System.out.println("The sum of "+ a + " and "+ b + " is "+ sum );
+	}
+}
