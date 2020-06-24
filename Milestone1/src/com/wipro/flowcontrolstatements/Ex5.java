7e9e8ec2bb59ffcfadadcd62c23253512039109f
Susrija <Susrija@DESKTOP-ILDABNP>
2020-06-24 12:20:29 +0530

    Java programs on Flow control statements

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex5.java
@@ -0,0 +1,17 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex5 {
+	public static void main(String args[])
+	{
+		int n;
+		n=Integer.parseInt(args[0]);
+		if(n%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+	}
+}
